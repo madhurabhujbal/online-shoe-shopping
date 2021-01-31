11e7e045a253de75e4ef6645ec3ec9ce8fd533f1
Madhura <madhura.bhujbal@gmail.com>
2021-01-31 16:32:58 +0000

    Render shoe images using the shoelist data

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get ("/", function (req, res) {
-    res.render ( "home.ejs", shoeList );
+    console.log(shoeList);
+    res.render ("home.ejs", shoeList);
 } );
 
 app.get("/url", (req, res) => {
