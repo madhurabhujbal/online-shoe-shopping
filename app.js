c34f6dc5415031976c11e970c5bbb585bf2266aa
Madhura <madhura.bhujbal@gmail.com>
2021-01-31 16:50:54 +0000

    Iterate over different shoe types

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get ("/", function (req, res) {
-    console.log(shoeList);
     res.render ("home.ejs", shoeList);
 } );
 
