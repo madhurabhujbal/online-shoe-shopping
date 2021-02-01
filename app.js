9f3705529c4da29282aafd448f86041ad2dd2c69
Madhura <madhura.bhujbal@gmail.com>
2021-02-01 23:31:06 +0000

    Refactor search operation

--- app.js
+++ app.js
@@ -10,21 +10,15 @@ app.get ("/", function (req, res) {
 } );
 
 app.get ("/details/:id", function (req, res) {
-    let shoeId = req.params.id;
-    let shoe;
-
-    let catagories = ["men", "women", "kids"];
-
-    for( let i = 0; i < catagories.length; i++) {
-        let catagory = catagories[i];
-        for(let j = 0; j < shoeList[catagory].length; j++) {
-            if(shoeList[catagory][j].id == shoeId){
-                shoe = shoeList[catagory][j];
+    let shoeInfo;
+    for(let catagory in shoeList) {
+        shoeList[catagory].forEach(shoe => {
+            if(shoe.id == req.params.id) {
+                shoeInfo = shoe;
             }
-        }
-    }
-    console.log(shoe);
-    res.render ("details.ejs", shoe);
+        });
+    };
+    res.render ("details.ejs", shoeInfo);
 } );
 
 app.get("/url", (req, res) => {
