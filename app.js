9c0e185e8e27b48634c6f8fa7b4d8bc0287ab75f
Madhura <madhura.bhujbal@gmail.com>
2021-02-01 23:12:50 +0000

    Give details of the shoes that is clicked

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const path = require('path');
-const shoeList = require('./data');
+const {shoeList} = require('./data');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get ("/", function (req, res) {
-    res.render ("home.ejs", shoeList);
+    res.render ("home.ejs", {shoeList});
 } );
 
 app.get ("/details/:id", function (req, res) {
-    res.render ("details.ejs", {id: req.params.id});
+    let shoeId = req.params.id;
+    let shoe;
+
+    let catagories = ["men", "women", "kids"];
+
+    for( let i = 0; i < catagories.length; i++) {
+        let catagory = catagories[i];
+        for(let j = 0; j < shoeList[catagory].length; j++) {
+            if(shoeList[catagory][j].id == shoeId){
+                shoe = shoeList[catagory][j];
+            }
+        }
+    }
+    console.log(shoe);
+    res.render ("details.ejs", shoe);
 } );
 
 app.get("/url", (req, res) => {
