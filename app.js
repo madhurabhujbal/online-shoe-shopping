ec52fc1f6c8ea7e2b2e358947e89e08b2fe04878
Madhura <madhura.bhujbal@gmail.com>
2021-02-03 22:58:48 +0000

    Add preliminary login mechanism without authenticaion

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const path = require('path');
 const {shoeList} = require('./data');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cookieParser());
+app.use(session({secret: "its a secret!",
+                 saveUninitialized: false,
+                 resave: false}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("/", function (req, res) {
@@ -13,11 +19,19 @@ app.get("/", function (req, res) {
 } );
 
 app.get("/sign-in", function (req, res) {
-    res.render ("signin.ejs");
+    let user = req.session.user;
+    if(!user) {
+        res.render ("signin.ejs");
+    } else {
+        res.send(`You are already logged in as ${user}`);
+    }
 } );
 
 app.post("/sign-in", function (req, res) {
-    res.send(`Signed in with username ${req.body.user}`);
+    let user = req.body.user;
+    // TODO: authenticate user here
+    req.session.user = user;
+    res.send(`Signed in successfully with username ${req.body.user}`);
 } );
 
 app.get ("/details/:id", function (req, res) {
