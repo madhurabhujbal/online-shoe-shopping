043e03a38b18921bcad2687cf1d24049ded70028
Madhura <madhura.bhujbal@gmail.com>
2021-02-09 20:37:08 +0000

    Add tests for userService.getUser

--- test/userSericeTest.js
+++ test/userSericeTest.js
@@ -1,7 +1,7 @@
 let assert = require('assert');
 let userService = require('../services/userService');
 
-describe('While retrieving user info', function () {
+describe('While validating user', function () {
     describe('When Passed correct username and password', function () {
         let userInfo = userService.validateUser('test', 'test');
         it('info of the user should be retrieved', function () {
@@ -30,6 +30,35 @@ describe('While retrieving user info', function () {
 
     describe('When Passed incorrect username', function () {
         let userInfo = userService.validateUser('nonExistingUser', 'doesNotMatter');
+        it('no user should be retrieved', function () {
+                assert.strictEqual(userInfo, undefined);
+        });
+    });
+});
+
+
+describe('While retrieving user info', function () {
+    describe('When Passed correct username', function () {
+        let userInfo = userService.getUser('test');
+        it('user info object should be retrieved', function () {
+                assert.notStrictEqual(userInfo, undefined);
+        });
+
+        it('info should contain username', function () {
+            assert.notStrictEqual(userInfo.username, undefined);
+        });
+
+        it('info should contain name', function () {
+            assert.notStrictEqual(userInfo.name, undefined);
+        });
+
+        it('info should contain previous orders', function () {
+            assert.notStrictEqual(userInfo.orders, undefined);
+        });
+    });
+
+    describe('When Passed non-existing username', function () {
+        let userInfo = userService.getUser('nonExistingUser');
         it('no user should be retrieved', function () {
                 assert.strictEqual(userInfo, undefined);
         });
