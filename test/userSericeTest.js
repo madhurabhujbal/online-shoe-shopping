2e66a19568a06b10dab75bab0e2adc0bd1793d25
Madhura <madhura.bhujbal@gmail.com>
2021-02-09 20:32:56 +0000

    Add tests for userService.validateUser

--- test/userSericeTest.js
+++ test/userSericeTest.js
@@ -4,7 +4,7 @@ let userService = require('../services/userService');
 describe('While retrieving user info', function () {
     describe('When Passed correct username and password', function () {
         let userInfo = userService.validateUser('test', 'test');
-        it('info of the user should be be retrieved', function () {
+        it('info of the user should be retrieved', function () {
                 assert.notStrictEqual(userInfo, undefined);
         });
 
@@ -18,6 +18,20 @@ describe('While retrieving user info', function () {
 
         it('info should contain previous orders', function () {
             assert.notStrictEqual(userInfo.orders, undefined);
+        });
+    });
+
+    describe('When Passed incorrect password', function () {
+        let userInfo = userService.validateUser('test', 'wrongPassword');
+        it('no user info should be retrieved', function () {
+                assert.strictEqual(userInfo, undefined);
+        });
+    });
+
+    describe('When Passed incorrect username', function () {
+        let userInfo = userService.validateUser('nonExistingUser', 'doesNotMatter');
+        it('no user should be retrieved', function () {
+                assert.strictEqual(userInfo, undefined);
         });
     });
 });
