d7fe729874c5a8f22ff0a8d98abf52a69bf5c339
Madhura Bhujbal <madhura.bhujbal@and.digital>
2021-05-02 15:41:28 +0100

    Code for listing all databases in the cluster

--- dbConnection/connection.js
+++ dbConnection/connection.js
@@ -2,8 +2,25 @@ const {MongoClient} = require("mongodb");
 
 async function main() {
     const uri = "mongodb+srv://madhura:mongodbAtlasPassword@goldenshoefreecluster.m5u7k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    await client.connect();
+
+    async function listDatabases(client) {
+        dbList = await client.db().admin().listDatabases();
+        console.log("Databases: ");
+        dbList.databases.forEach(db => console.log(` - ${db.name}`));
+    }
+
+    try {
+        await client.connect();
+        console.log("connection to db established");
+        await listDatabases(client);
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
 }
+
+main().catch(console.error);
+
 
